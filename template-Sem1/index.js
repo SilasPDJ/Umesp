3303fc2e2b505cc9a1970962690fef6a81460976
PDJ <oesksbfserver@gmail.com>
2021-06-15 03:29:02 -0300

    juntando-pt328

--- template-Sem1/index.js
+++ template-Sem1/index.js
@@ -1,6 +1,23 @@
 const prefix = "/produtos/"
 let link_prefix = [".html"][0]
-const links = ["domestico" + link_prefix, "moda" + link_prefix, "variados" + link_prefix]
+let links = ["domestico", "moda", "variados"]
+
+links = links.map(function (val, e) {
+    return val + link_prefix;
+})
 let compras = document.querySelector("#comprasSorted")
 compras.setAttribute("href", prefix + links[Math.floor(Math.random() * links.length)])
 // Randomiza página de produtos
+
+
+
+const imgPro = document.getElementById("add-pro-imgUploaded")
+const btPro = document.getElementById("add-pro-adicionarArquivo")
+
+btPro.addEventListener("change", function (event) {
+    imgPro.style = 'max-width:100%; max-height:90%;'
+    imgPro.src = URL.createObjectURL(this.files[0]); // set src to blob url
+
+})
+
+//AdicionaImagens add-pro.html
